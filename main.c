d2cdd78d0296048b4f47161a27bd3ee89a3f06fd
crivero333 <crivero333@proton.me>
2026-08-26 21:35:46 -0400

    files are managed by main and passed by reference to the functions that use them

--- main.c
+++ main.c
@@ -7,15 +7,13 @@
 #include <stdio.h>
 #include <string.h>
 
-FILE *input = NULL;
-FILE *output = NULL;
-
-int putcmd(char);
-void init_program(void);
-void end_program(void);
+int putcmd(FILE*, char);
+void init_program(FILE*);
+void end_program(FILE*);
 
 int main(int argc, char *argv[]) {
-    /** argument checking\ @todo make it support at max 4 args */
+    FILE *input = NULL, *output = NULL;
+    /** argument checking\ @todo make it support at max 4 args **/
     if(argc != 4) {
         fprintf(stderr, "usage: brfktoc file -o output.c");
         return 1;
@@ -23,9 +21,9 @@ int main(int argc, char *argv[]) {
     int i;
     for(i = 1; i < argc; i++) {
         int is_o = strcmp(argv[i], "-o") == 0;
-        // when "-o" found between pos 1 and 2
+        /** when "-o" found between pos 1 and 2 **/ 
         if(i != 3 && is_o) {
-            // go to the next argument and open the file
+            /** go to the next argument and open the file **/
             ++i;
             output = fopen(argv[i], "w");
             if(output == NULL) {
@@ -33,12 +31,12 @@ int main(int argc, char *argv[]) {
                 return 1;
             }
         }
-        // when "-o" is the last parameter
+        /** when "-o" is the last parameter **/
         else if(i == 3 && is_o) {
             fprintf(stderr, "error: output argument declared but never read\n");
         }
         else {
-            // and input file is already loaded
+            /** an input file is already loaded **/
             if(input != NULL) {
                 fprintf(stderr, "error: more than one input file declared\n");
                 return 1;
@@ -51,54 +49,55 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // constructing the final program
-    init_program();
+    /** translating the program **/
+    init_program(output);
     int c;
     while((c = getc(input)) != EOF) {
-        int puterr = putcmd(c);
+        int puterr = putcmd(output, c);
         if(puterr > 0) {
             fclose(input);
             fclose(output);
             return puterr;
         }
     }
-    end_program();
+    end_program(output);
 
     fclose(input);
     fclose(output);
     return 0;
 }
-int putcmd(char c) {
+int putcmd(FILE* outfile, char c) {
+    /** keeps track of the closing of the loops */
     static int loopstack = 0;
     switch(c) {
     case '<':
-        for(int i = 0; i < loopstack+1; i++) fprintf(output, "\t");
-        fprintf(output, "move_left();\n");
+        for(int i = 0; i < loopstack+1; i++) fprintf(outfile, "\t");
+        fprintf(outfile, "move_left();\n");
         return 0;
     case '>':
-        for(int i = 0; i < loopstack+1; i++) fprintf(output, "\t");
-        fprintf(output, "move_right();\n");
+        for(int i = 0; i < loopstack+1; i++) fprintf(outfile, "\t");
+        fprintf(outfile, "move_right();\n");
         return 0;
     case '.':
-        for(int i = 0; i < loopstack+1; i++) fprintf(output, "\t");
-        fprintf(output, "put_current();\n");
+        for(int i = 0; i < loopstack+1; i++) fprintf(outfile, "\t");
+        fprintf(outfile, "put_current();\n");
         return 0;
     case ',':
-        for(int i = 0; i < loopstack+1; i++) fprintf(output, "\t");
-        fprintf(output, "get_current();\n");
+        for(int i = 0; i < loopstack+1; i++) fprintf(outfile, "\t");
+        fprintf(outfile, "get_current();\n");
         return 0;
     case '+':
-        for(int i = 0; i < loopstack+1; i++) fprintf(output, "\t");
-        fprintf(output, "increment();\n");
+        for(int i = 0; i < loopstack+1; i++) fprintf(outfile, "\t");
+        fprintf(outfile, "increment();\n");
         return 0;
     case '-':
-        for(int i = 0; i < loopstack+1; i++) fprintf(output, "\t");
-        fprintf(output, "decrement();\n");
+        for(int i = 0; i < loopstack+1; i++) fprintf(outfile, "\t");
+        fprintf(outfile, "decrement();\n");
         return 0;
     case '[':
-        for(int i = 0; i < loopstack+1; i++) fprintf(output, "\t");
+        for(int i = 0; i < loopstack+1; i++) fprintf(outfile, "\t");
         loopstack++; // adds loop to the stack
-        fprintf(output, "while(*current) {\n");
+        fprintf(outfile, "while(*current) {\n");
         return 0;
     case ']':
         if(loopstack == 0) {
@@ -106,43 +105,43 @@ int putcmd(char c) {
             return 1;
         }
         loopstack--; // removes loop
-        for(int i = 0; i < loopstack+1; i++) fprintf(output, "\t");
-        fprintf(output, "}\n");
+        for(int i = 0; i < loopstack+1; i++) fprintf(outfile, "\t");
+        fprintf(outfile, "}\n");
         return 0;
     default:
         fprintf(stderr, "error: unrecognized command %c\n", c);
         return 1;
     }
 }
-void init_program(void) {
-    fprintf(output, "#include <stdio.h>\n");
-    fprintf(output, "#define MAXMEM 30000\n");
-    fprintf(output, "unsigned char MEM[MAXMEM];\n");
-    fprintf(output, "unsigned char *current = MEM;\n");
-    fprintf(output, "void init() {\n");
-    fprintf(output, "\tint i = 0;\n");
-    fprintf(output, "\tfor(i = 0; i < MAXMEM; i++) MEM[i] = 0;\n");
-    fprintf(output, "}\n");
-    fprintf(output, "void increment() {(*current)++;}\n");
-    fprintf(output, "void decrement() {(*current)--;}\n");
-    fprintf(output, "void move_left() {\n");
-    fprintf(output, "\tunsigned char* right = MEM+MAXMEM-1;\n");
-    fprintf(output, "\tunsigned char* left = MEM;\n");
-    fprintf(output, "\tif(current == left) current = right;\n");
-    fprintf(output, "\telse current--;\n");
-    fprintf(output, "}\n");
-    fprintf(output, "void move_right() {\n");
-    fprintf(output, "\tunsigned char* right = MEM+MAXMEM;\n");
-    fprintf(output, "\tunsigned char* left = MEM;\n");
-    fprintf(output, "\tif(current == right-1) current = left;\n");
-    fprintf(output, "\telse current++;\n");
-    fprintf(output, "}\n");
-    fprintf(output, "void put_current() {putchar(*current);}\n");
-    fprintf(output, "void get_current() { *current = getchar(); }\n");
-    fprintf(output, "int main() {\n");
-    fprintf(output, "\tinit();\n");
+void init_program(FILE* outfile) {
+    fprintf(outfile, "#include <stdio.h>\n\
+        #define MAXMEM 30000\n\
+        unsigned char MEM[MAXMEM];\n\
+        unsigned char *current = MEM;\n\
+        void init() {\n\
+        \tint i = 0;\n\
+        \tfor(i = 0; i < MAXMEM; i++) MEM[i] = 0;\n\
+        }\n\
+        void increment() {(*current)++;}\n\
+        void decrement() {(*current)--;}\n\
+        void move_left() {\n\
+        \tunsigned char* right = MEM+MAXMEM-1;\n\
+        \tunsigned char* left = MEM;\n\
+        \tif(current == left) current = right;\n\
+        \telse current--;\n\
+        }\n\
+        void move_right() {\n\
+        \tunsigned char* right = MEM+MAXMEM;\n\
+        \tunsigned char* left = MEM;\n\
+        \tif(current == right-1) current = left;\n\
+        \telse current++;\n\
+        }\n\
+        void put_current() {putchar(*current);}\n\
+        void get_current() { *current = getchar(); }\n\
+        int main() {\n\
+        \tinit();\n");
 }
-void end_program(void) {
-    fprintf(output, "\treturn 0;\n");
-    fprintf(output, "}");
+void end_program(FILE* outfile) {
+    fprintf(outfile, "\treturn 0;\n");
+    fprintf(outfile, "}");
 }
