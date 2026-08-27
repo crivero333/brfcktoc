ed5a0acba4a8bdf77db42ae6229cde0e17d42533
crivero333 <crivero333@proton.me>
2026-08-27 12:44:10 -0400

    fiexed minor issue with the header file

--- main.c
+++ main.c
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
 
     FILE *headerfile = NULL;
     /** loading header file containing logic, somehow actual header file can lead to errors? **/
-    headerfile = fopen("brfk_logic.c", "r");
+    headerfile = fopen("brfck_logic.c", "r");
     if(headerfile == NULL) {
         fprintf(stderr, "error: could not load header file\n");
         return 1;
