96d62301eb611f7d38c6b2205b1fa76c0ebcc839
crivero333 <crivero333@proton.me>
2026-08-26 23:16:52 -0400

    now the program can accept 1 parameter as well as 4 parameters

--- main.c
+++ main.c
@@ -13,47 +13,61 @@ void end_program(FILE*);
 
 int main(int argc, char *argv[]) {
     FILE *input = NULL, *output = NULL;
-
-    /** argument checking\ @todo make it support at max 4 args **/
-    if(argc != 4) {
-        fprintf(stderr, "usage: brfktoc file -o output.c\n");
-        return 1;
-    }
-    int i;
-    for(i = 1; i < argc; i++) {
-        int is_o = strcmp(argv[i], "-o") == 0;
-        /** when "-o" found between pos 1 and 2 **/ 
-        if(i != 3 && is_o) {
-            /** go to the next argument and open the file **/
-            ++i;
-            output = fopen(argv[i], "w");
-            if(output == NULL) {
-                if(input != NULL)
-                    fclose(input);
-                fprintf(stderr, "error: could not load output file\n");
-                return 1;
-            }
-        }
-        /** when "-o" is the last parameter **/
-        else if(i == 3 && is_o) {
-            if(input != NULL)
-                fclose(input);
-            fprintf(stderr, "error: output argument declared but never read\n");
+    
+    /** only one parameter, for input and output **/
+    if(argc == 2) {
+        input = fopen(argv[1], "r");
+        if(input == NULL) {
+            fprintf(stderr, "error: could not load the input file\n");
             return 1;
         }
-        else {
-            /** an input file is already loaded **/
-            if(input != NULL) {
-                fclose(input);
-                fprintf(stderr, "error: more than one input file declared\n");
+        output = fopen(argv[1], "w+");
+    }
+    /** all parameters possible **/
+    else if(argc == 4) {
+        int i;
+        for(i = 1; i < argc; i++) {
+            int is_o = strcmp(argv[i], "-o") == 0;
+            /** when "-o" found between pos 1 and 2 **/ 
+            if(i != 3 && is_o) {
+                /** go to the next argument and open the file, correct **/
+                ++i;
+                output = fopen(argv[i], "w+");
+                if(output == NULL) {
+                    if(input != NULL)
+                        fclose(input);
+                    fprintf(stderr, "error: could not load output file\n");
+                    return 1;
+                }
+            }
+            /** when "-o" is the last parameter, bad **/
+            else if(i == 3 && is_o) {
+                if(input != NULL)
+                    fclose(input);
+                fprintf(stderr, "error: output argument declared but never read\n");
                 return 1;
             }
-            input = fopen(argv[i], "r");
-            if(input == NULL) {
-                fprintf(stderr, "error: could not load input file\n");
-                return 1;
+            else {
+                /** an input file is already loaded, bad **/
+                if(input != NULL) {
+                    fclose(input);
+                    fprintf(stderr, "error: more than one input file declared\n");
+                    return 1;
+                }
+                input = fopen(argv[i], "r");
+                if(input == NULL) {
+                    fprintf(stderr, "error: could not load input file\n");
+                    return 1;
+                }
             }
         }
+    }
+    /** missing parameters **/
+    else {
+        fprintf(stderr, "\
+            usage: brfcktoc input_file\n\
+                   brfcktoc input_file -o output_file.c\n");
+        return 1;
     }
 
     FILE *headerfile = NULL;
@@ -129,6 +143,11 @@ int putcmd(FILE* outfile, char c) {
     }
 }
 void init_program(FILE* outfile, FILE* logicheader) {
+    /**
+    @todo have to find some efficient way to copy the contents
+          from the logicheader to the outfile. ideally in a
+          direct manner
+    */
     char initbuffer[630];
     fscanf(logicheader, "%s", initbuffer);
     fprintf(outfile, "%s\n\
